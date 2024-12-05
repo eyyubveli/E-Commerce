093d25156ab81de408ebd0e239f88c0b41e32e0a
eyyubveli <eyub.050@gmail.com>
2024-12-05 12:24:19 -0800

    :hammer: During the searching process, the relevant page was redirected.

--- src/Components/SearchingPlants/SearchingPlants.jsx
+++ src/Components/SearchingPlants/SearchingPlants.jsx
@@ -1,28 +1,32 @@
 import React from 'react'
 import { plantersItems, saleItems, trendingItems } from '../../items/items'
 import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 
 const SearchingPlants = () => {
     const inputValue = useSelector(state => state.products.inputValue);
     const allItems = [...plantersItems, ...saleItems, ...trendingItems];
-    
+    const navigate = useNavigate();
     if (!inputValue) return;
 
     const matchingItems = allItems.filter(item => item.title.toLowerCase().includes(inputValue.toLowerCase()));
 
-    console.log(matchingItems);
-
+    const handleClick = (id) => {        
+        if (id) {
+            navigate(`/product-details/${id}`);
+        }
+    }
 
     return (
         <div className="matching">
             {
-                 matchingItems.length > 0 ? 
+                matchingItems.length > 0 ?
                     matchingItems.map(item =>
-                        <div className="items" key={item.id}>
+                        <div className="items" key={item.id} onClick={() => handleClick(item.id)}>
                             <img width={80} height={80} src={item.img} alt={item.alt} title={item.title} />
                             <span>{item.title}</span>
                         </div>
-                    )  : (<p>No matching items</p>)
+                    ) : (<p>No matching items</p>)
             }
         </div>
     )
